d8c036908726485dc4be3ea3260780acee6290b4
developer006 <developer.ludovic@gmail.com>
2023-05-08 23:35:30 +0300

    message report added

--- resources/views/app/sent_messages/create.blade.php
+++ resources/views/app/sent_messages/create.blade.php
@@ -4,12 +4,12 @@
 <div class="container-fluid"> <div class="row page-titles mx-0">
         <div class="col-sm-6 p-md-0">
             <div class="welcome-text">
-                <h4> @lang('crud.services.create_title')</h4>
+                <h4> @lang('crud.services.sent-messages')</h4>
             </div>
         </div>
         <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{url('services')}}">services</a></li>
+                <li class="breadcrumb-item"><a href="{{url('sent-messages')}}">services</a></li>
                 <li class="breadcrumb-item active"><a href="javascript:void(0)">create</a></li>
             </ol>
         </div>
